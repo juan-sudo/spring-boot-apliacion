db1efe67421a777dbb44304ff3220ac8ac17d477
51955 <51955@DESKTOP-5V97HOC>
2019-07-30 16:59:15 -0500

    comit iniacial con eclipse

---
+++ Grencosur-inventario/src/test/java/com/inventario/proyecto/GrencosurInventarioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inventario.proyecto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrencosurInventarioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
